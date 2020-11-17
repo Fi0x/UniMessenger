35a6ae2d6595492c4e242fba7b38ebd5ade5b2c6
Fi0x <felpech@aol.com>
2020-11-17 12:00:15 +0100

    Fixed 406 response code by changing html to json
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/unimessenger/userinteraction/menu/MenuWireOverview.java
+++ src/main/java/unimessenger/userinteraction/menu/MenuWireOverview.java
@@ -54,7 +54,7 @@ public class MenuWireOverview
         //TODO: Show a list of all Wire-conversations
         String url = Variables.URL_WIRE + Commands.CONVERSATIONS + "?access_token=" + Storage.wireBearerToken;
         String[] headers = new String[]{
-                "accept", "text/html"};
+                "accept", "application/json"};
         HttpResponse<String> response = CLI.userHTTP.sendRequest(url, Variables.REQUESTTYPE.GET, "", headers);
         System.out.println("Response code: " + response.statusCode());
         System.out.println("Headers:" + response.headers());
