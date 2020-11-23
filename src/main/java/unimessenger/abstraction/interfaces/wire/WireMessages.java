8e413957ef11cc753cbc70f70aaf1e9bfc779875
Fi0x <felpech@aol.com>
2020-11-23 19:25:31 +0100

    Made Message sending work
    Added response check for sent messages
    Sending correct client ids as recipients in message body
    Added helper method to get client IDs of a user
    Changed chat id of test message
    Disabled persistent login for autologin
    Added new URL string "users"
    Enhaced for loop in WireData class
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/unimessenger/abstraction/interfaces/wire/WireMessages.java
+++ src/main/java/unimessenger/abstraction/interfaces/wire/WireMessages.java
@@ -1,11 +1,15 @@
 package unimessenger.abstraction.interfaces.wire;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import unimessenger.abstraction.Headers;
 import unimessenger.abstraction.URL;
 import unimessenger.abstraction.interfaces.IMessages;
 import unimessenger.abstraction.storage.WireStorage;
 import unimessenger.apicommunication.HTTP;
+import unimessenger.userinteraction.Outputs;
 import unimessenger.util.enums.REQUEST;
 
 import java.lang.constant.Constable;
@@ -17,7 +21,7 @@ import java.util.Map;
 public class WireMessages implements IMessages
 {
     @Override
-    public boolean sendMessage(String chatID, String text)//TODO: Make this method work
+    public boolean sendMessage(String chatID, String text)
     {
         String url = URL.WIRE + URL.WIRE_CONVERSATIONS + "/" + chatID + URL.WIRE_OTR_MESSAGES + URL.WIRE_TOKEN + WireStorage.getBearerToken();
         String[] headers = new String[]{
@@ -26,9 +30,14 @@ public class WireMessages implements IMessages
         String body = buildBody(chatID, text);
         HttpResponse<String> response = new HTTP().sendRequest(url, REQUEST.POST, body, headers);
 
-        System.out.println("Response code: " + response.statusCode());
-        System.out.println("Headers: " + response.headers());
-        System.out.println("Body: " + response.body());
+
+        if(response == null) Outputs.printError("No response for sent message");
+        else if(response.statusCode() == 201)
+        {
+            Outputs.printInfo("Message sent correctly");
+            return true;
+        } else Outputs.printError("Response code was " + response.statusCode());
+
         return false;
     }
 
@@ -40,21 +49,56 @@ public class WireMessages implements IMessages
         obj.put("sender", WireStorage.clientID);
 
         ArrayList<String> members = new WireData().getConversationMembersFromID(chatID);
-        if(members.size() > 0) members.remove(0);
-        Map<String, Map<String, Constable>> recipients = new LinkedHashMap<>(1);
-        Map<String, Constable> clientMap = new LinkedHashMap<>(members.size());
+        JSONObject recipients = new JSONObject();
 
         for(String id : members)
         {
-            clientMap.put("", id);
+            Map<String, Constable> clientMap = new LinkedHashMap<>(members.size());
+            ArrayList<String> userClients = getClientIDsFromUser(id);
+            while(!userClients.isEmpty())
+            {
+                if(!(id.equals(WireStorage.userID) && userClients.get(0).equals(WireStorage.clientID)))
+                {
+                    //TODO: Use correct OTR Content
+                    clientMap.put(userClients.get(0), "OTR Content");
+                }
+                userClients.remove(0);
+            }
+            recipients.put(id, clientMap);
         }
 
-        recipients.put("", clientMap);
-        //TODO: Add recipient UUIDs
         obj.put("recipients", recipients);
 
-        System.out.println("Body to send: " + obj.toJSONString());
         return obj.toJSONString();
+    }
+
+    private static ArrayList<String> getClientIDsFromUser(String userID)
+    {
+        String url = URL.WIRE + URL.WIRE_USERS + "/" + userID + URL.WIRE_CLIENTS + URL.WIRE_TOKEN + WireStorage.getBearerToken();
+        String[] headers = new String[]{
+                Headers.ACCEPT_JSON[0], Headers.ACCEPT_JSON[1]};
+        HttpResponse<String> response = new HTTP().sendRequest(url, REQUEST.GET, "", headers);
+
+        if(response == null) Outputs.printDebug("No client response");
+        else if(response.statusCode() == 200)
+        {
+            try
+            {
+                JSONArray arr = (JSONArray) new JSONParser().parse(response.body());
+                ArrayList<String> ids = new ArrayList<>();
+                while(!arr.isEmpty())
+                {
+                    JSONObject obj = (JSONObject) arr.get(0);
+                    ids.add(obj.get("id").toString());
+                    arr.remove(0);
+                }
+                return ids;
+            } catch(ParseException ignored)
+            {
+            }
+        } else Outputs.printError("Response code is " + response.statusCode());
+
+        return null;
     }
 
     @Deprecated
