4f121d207c40b46c0bd1d9de9507d95b79a87b8c
Fi0x <felpech@aol.com>
2020-11-26 20:06:25 +0100

    Tested the payload of a notification
    Added notification example
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/unimessenger/abstraction/interfaces/wire/WireMessageReceiver.java
+++ src/main/java/unimessenger/abstraction/interfaces/wire/WireMessageReceiver.java
@@ -13,6 +13,7 @@ import unimessenger.userinteraction.Outputs;
 import unimessenger.util.enums.REQUEST;
 
 import java.net.http.HttpResponse;
+import java.util.Set;
 import java.util.UUID;
 
 public class WireMessageReceiver
@@ -31,7 +32,36 @@ public class WireMessageReceiver
             Outputs.create("No response received", this.getClass().getName()).debug().WARNING().print();
         else if(response.statusCode() == 200 || response.statusCode() == 404)
         {
-            System.out.print("Body: " + response.body());
+            try
+            {
+                JSONObject obj = (JSONObject) new JSONParser().parse(response.body());
+                JSONArray arr = (JSONArray) obj.get("notifications");
+                for(Object o : arr)
+                {
+                    JSONObject note = (JSONObject) o;
+                    JSONArray payload = (JSONArray) note.get("payload");
+                    for(Object pl : payload)//Contains: TYPE, client, data, from, time, conversation, connection, user, value, key
+                    {
+                        JSONObject load = (JSONObject) pl;
+                        Set keys = load.keySet();
+                        if(keys.contains("type"))
+                        {
+//                            if(keys.contains("client")) System.out.println("Client: " + load.get("type"));
+//                            if(keys.contains("data")) System.out.println("Data: " + load.get("type"));
+//                            if(keys.contains("from")) System.out.println("From: " + load.get("type"));
+//                            if(keys.contains("time")) System.out.println("Time: " + load.get("type"));
+//                            if(keys.contains("conversation")) System.out.println("Conversation: " + load.get("type"));
+//                            if(keys.contains("connection")) System.out.println("Connection: " + load.get("type"));
+//                            if(keys.contains("user")) System.out.println("User: " + load.get("type"));
+//                            if(keys.contains("value")) System.out.println("Value: " + load.get("type"));
+//                            if(keys.contains("key")) System.out.println("Key: " + load.get("type"));
+                        }
+                    }
+                }
+            } catch(ParseException ignored)
+            {
+                Outputs.create("Something went wrong when parsing the HTTP response", this.getClass().getName()).debug().WARNING();
+            }
             return true;
         } else Outputs.create("Response code was " + response.statusCode(), this.getClass().getName()).debug().WARNING().print();
         return false;
@@ -50,13 +80,9 @@ public class WireMessageReceiver
         {
             JSONObject temp = (JSONObject) new JSONParser().parse(response.body());
             String pl = temp.get("payload").toString();
-            System.out.println("PayLoad: " + pl);
             JSONArray payLArr = (JSONArray) new JSONParser().parse(pl);
             JSONObject payL = (JSONObject) new JSONParser().parse(payLArr.get(0).toString());
-            System.out.println("From: " + payL.get("from").toString());
             JSONObject data = (JSONObject) new JSONParser().parse(payL.get("data").toString());
-            System.out.println("Text: " + data.get("text"));
-            System.out.println("Sender: " + data.get("sender"));
             String ret = WireCryptoHandler.decrypt(UUID.fromString(payL.get("from").toString()), data.get("sender").toString(), data.get("text").toString());
             System.out.println("TextInGut: " + ret);
 
